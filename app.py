a210c890dae84644b9605f55030ddab9885da0f5
Albert Medrano-Lopez <albert@mindcontainer.com>
2022-06-19 09:09:41 -0400

    First template page

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask import render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello Flask!'
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name = None):
+    return render_template(
+        "hello.html",
+        name=name,
+        date=datetime.now()
+    )
